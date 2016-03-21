30ef49f9402cd6eb3004642ab0b29dc8c38e316e
Ben Mz <benoit@webboards.fr>
2016-03-21 14:13:54 +0100

    Add Http and crypto.js/SHA1 package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'benmz:livetoken-base',
-  version: '0.1.3',
+  version: '0.1.4',
   summary: 'Basic methods for LiveToken.io interaction (token authentication system)',
   git: 'https://github.com/x62en/livetoken-base.git',
   documentation: 'README.md'
@@ -15,7 +15,10 @@ Package.onUse(function(api) {
   api.use('coffeescript');
   api.use('session', 'client');
   api.use('check', 'server');
+  api.use('http', 'server');
   api.use('meteorhacks:async@1.0.0', 'server');
+  api.use('jparker:crypto-sha1@0.1.0', 'server');
+
 
   // The files of this package
   api.addFiles('livetoken-base.coffee');
