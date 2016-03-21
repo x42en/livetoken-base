8a1368f83c3b3b40a47da1d5e0fbf07aa1309872
Ben Mz <benoit@webboards.fr>
2016-03-21 13:33:15 +0100

    Add session and check package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'benmz:livetoken-base',
-  version: '0.1.0',
+  version: '0.1.2',
   summary: 'Basic methods for LiveToken.io interaction (token authentication system)',
   git: 'https://github.com/x62en/livetoken-base.git',
   documentation: 'README.md'
@@ -13,6 +13,8 @@ Package.onUse(function(api) {
   // Core packages and 3rd party packages
   api.use('accounts-base');
   api.use('coffeescript');
+  api.use('session', 'client');
+  api.use('check', 'server');
 
   // The files of this package
   api.addFiles('livetoken-base.coffee');
@@ -23,5 +25,6 @@ Package.onTest(function(api) {
   api.use('tinytest');
   api.use('benmz:livetoken-base');
   api.use('coffeescript');
+
   api.addFiles('livetoken-base-tests.js');
 });
